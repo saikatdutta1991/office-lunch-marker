61389d5ab44b916c0393544cb8ad4a3b38e07a45
Saikat LiftOff <60886046+saikat-liftoffllc@users.noreply.github.com>
2022-07-05 23:47:39 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+
+const SLACK_APP_TOKEN = process.env.SLACK_APP_TOKEN;
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+const PORT = process.env.PORT;
+
+const app = new App({
+  token: SLACK_BOT_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET,
+  appToken: SLACK_APP_TOKEN,
+  socketMode: true,
+  port: PORT || 3000,
+});
+
+app.message("hello", async ({ message, say }) => {
+  console.log(`hello message received`);
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  await app.start();
+  console.log("⚡️ Bolt app is running!");
+})();
