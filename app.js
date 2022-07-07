fd3c08d0751a608335aa9ef8eebcd8c86403a29c
Saikat LiftOff <60886046+saikat-liftoffllc@users.noreply.github.com>
2022-07-07 16:36:17 +0530

    config fix

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const botApp = require("./bot-app");
 const authApp = require("./auth-app");
 
--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
+
 const config = {
   port: process.env.PORT | "3000",
   redisUrl: process.env.REDIS_URL,
