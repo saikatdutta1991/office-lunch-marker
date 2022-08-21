76902ff4a285b9cc59af3d88b1d09474985d00ed
Saikat LiftOff <60886046+saikat-liftoffllc@users.noreply.github.com>
2022-08-22 00:19:37 +0530

    Server stopped

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const botApp = require("./bot-app");
 const authApp = require("./auth-app");
 
 (async () => {
-  await botApp.start();
-  await authApp.start();
-  console.log("⚡️ Bolt app is running!");
+  // await botApp.start();
+  // await authApp.start();
+  // console.log("⚡️ Bolt app is running!");
+  console.log(`Server is stopped`);
 })();
